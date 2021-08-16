f33a481ce6b312e029a07eff66792d2fad5c38d7
Tamara Nawrocki <tammy@Tamaras-MacBook-Pro.local>
2021-08-15 22:08:00 -0500

    Send Emails

--- main.js
+++ main.js
@@ -12,25 +12,24 @@ function sendEmail() {
   }).then((message) => alert("mail sent successfully"));
 }
 
-const date = document.querySelector("#fecha");
-
-const nombre = document.querySelector("#nombres").textContent;
-
-const age = document.querySelector("#años").textContent;
-
-const email = document.querySelector("#email").textContent;
-
-const lastNames = document.querySelector("#apellidos").textContent;
-
-const gender = document.querySelector("#genero").value;
-
-const questionOne = document.querySelector("#pregunta-1").textContent;
-
-const questionTwo = document.querySelector("#pregunta-2").textContent;
-
-const questionThree = document.querySelector("#pregunta-3").textContent;
-
 const personalInfo = () => {
+  const date = document.querySelector("#fecha").value;
+
+  const nombre = document.querySelector("#nombres").value;
+
+  const age = document.querySelector("#años").value;
+
+  const email = document.querySelector("#email").value;
+
+  const lastNames = document.querySelector("#apellidos").value;
+
+  const gender = document.querySelector("#genero").value;
+
+  const questionOne = document.querySelector("#pregunta-1").textContent;
+
+  const questionTwo = document.querySelector("#pregunta-2").textContent;
+
+  const questionThree = document.querySelector("#pregunta-3").textContent;
   console.log(
     date,
     nombre,
@@ -42,6 +41,23 @@ const personalInfo = () => {
     questionTwo,
     questionThree
   );
+  Email.send({
+    Host: "smtp.gmail.com",
+    Username: "61002176@alumnos.innovaschools.edu.pe",
+    Password: "gtcqifzcvdjifsbv",
+    To: "61002176@alumnos.innovaschools.edu.pe",
+    From: "61002176@alumnos.innovaschools.edu.pe",
+    Subject: "JobHunt trabajo",
+    Body: `Fecha: ${date}
+    \nNombres: ${nombre}
+    \nAños: ${age}
+    \nEmail: ${email}
+    \nApellidos: ${lastNames}
+    \nGénero: ${gender}
+    \nPregunta1: ${questionOne}
+    \nPregunta2: ${questionTwo}
+    \nPregunta3: ${questionThree}`,
+  }).then((message) => alert("mail sent successfully"));
 };
 
 const button = document.querySelector("#envio");
